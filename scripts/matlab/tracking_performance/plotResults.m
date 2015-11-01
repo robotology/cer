f7a505dd0ea647f6026eed3b8fa4a745c1f5c4fd
Ugo Pattacini <ugo.pattacini@iit.it>
2015-11-02 00:18:33 +0100

    orientations are plotted as well

--- scripts/matlab/tracking_performance/plotResults.m
+++ scripts/matlab/tracking_performance/plotResults.m
@@ -5,7 +5,7 @@ function plotResults(filename)
 
 global ha hxd hx
 global data
-global cnt line
+global cnt line A
 
 hfig=figure('Name','Tracking Performance','Color','w');
 set(hfig,'Toolbar','figure');
@@ -23,6 +23,7 @@ A=max(abs(lim))*0.1;
 quiver3(ha,0,0,0,1,0,0,A,'Color','r','Linewidth',2);
 quiver3(ha,0,0,0,0,1,0,A,'Color','g','Linewidth',2);
 quiver3(ha,0,0,0,0,0,1,A,'Color','b','Linewidth',2);
+A=A*0.75;
 
 hxd=[]; hx=[];
 data=importdata(filename);
@@ -40,7 +41,7 @@ function myCallback(obj,~,~)
 
 global ha hxd hx
 global data
-global cnt line
+global cnt line A
 
 cnt=cnt+1;
 period=get(obj,'Period');
@@ -66,9 +67,20 @@ if ~isempty(j)
     if ~isempty(hxd)
         delete(hxd);
     end
-
-    hxd=plot3(ha,data.data(j,2),data.data(j,3),data.data(j,4),'ro',...
-              'LineWidth',4,'MarkerSize',4);
+    
+    x=data.data(j,2:4);
+    u=data.data(j,5:7); n=norm(u);
+    H=axis2dcm([u/n n]);
+    
+    hxd=hggroup;
+    h1=plot3(ha,x(1),x(2),x(3),'ro','LineWidth',3,'MarkerSize',3);
+    h2=quiver3(ha,x(1),x(2),x(3),H(1,1),H(2,1),H(3,1),A,'r');
+    h3=quiver3(ha,x(1),x(2),x(3),H(1,2),H(2,2),H(3,2),A,'g');
+    h4=quiver3(ha,x(1),x(2),x(3),H(1,3),H(2,3),H(3,3),A,'b');
+    set(h1,'Parent',hxd);
+    set(h2,'Parent',hxd);
+    set(h3,'Parent',hxd);
+    set(h4,'Parent',hxd);
     drawnow;
 end
 
@@ -76,9 +88,20 @@ if ~isempty(k)
     if ~isempty(hx)
         delete(hx);
     end
+    
+    x=data.data(k,2:4);
+    u=data.data(k,5:7); n=norm(u);
+    H=axis2dcm([u/n n]);   
 
-    hx=plot3(ha,data.data(k,2),data.data(k,3),data.data(k,4),'bo',...
-             'LineWidth',4,'MarkerSize',4);
+    hx=hggroup;
+    h1=plot3(ha,x(1),x(2),x(3),'bo','LineWidth',3,'MarkerSize',3);
+    h2=quiver3(ha,x(1),x(2),x(3),H(1,1),H(2,1),H(3,1),A,'r');
+    h3=quiver3(ha,x(1),x(2),x(3),H(1,2),H(2,2),H(3,2),A,'g');
+    h4=quiver3(ha,x(1),x(2),x(3),H(1,3),H(2,3),H(3,3),A,'b');
+    set(h1,'Parent',hx);
+    set(h2,'Parent',hx);
+    set(h3,'Parent',hx);
+    set(h4,'Parent',hx);   
     drawnow;
 end
 
