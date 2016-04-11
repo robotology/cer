68df56d344c2a21f4cf2b514eadb9678ca436ab3
Alberto Cardellino <alberto.cardellino@iit.it>
2016-04-11 16:44:26 +0200

    Tests: introduces test for CER display. It uses all 3 methods to set image on the screen

---
+++ tests/src/displayTest/displayTest.h
@@ -0,0 +1,48 @@
+
+//  Copyright (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+//  Authors: Alberto Cardellino <alberto.cardellino@iit.it>
+//
+//  Copy Policy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+
+#ifndef DISPLAY_TEST_H
+#define DISPLAY_TEST_H
+
+#include <yarp/sig/Image.h>
+#include <yarp/os/RateThread.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/BufferedPort.h>
+
+#include <IFaceDisplayInterface.h>
+
+#include <rtf/yarp/YarpTestCase.h>
+
+/**
+* \ingroup cer-tests
+* Test basic functionalities of CER display device
+*
+*  Accepts the following parameters:
+* | Parameter name | Type   | Units | Default Value      | Required | Description                                        | Notes |
+* |:--------------:|:------:|:-----:|:------------------:|:--------:|:--------------------------------------------------:|:-----:|
+* | local          | string | -     | /displayTest       | No       | The prefix name of the local ports to open.        | -     |
+* | remote         | string | -     | /robot/faceDisplay | No       | The prefix name of the remote ports to connect to. | -     |
+*/
+
+class DisplayTest : public YarpTestCase
+{
+public:
+    DisplayTest();
+    virtual ~DisplayTest();
+
+    virtual bool setup(yarp::os::Property& property);
+    virtual void run();
+    virtual void tearDown();
+
+private:
+    yarp::sig::Image                image;
+    yarp::dev::PolyDriver           display;
+    cer::dev::IFaceDisplay          *iDisp;
+
+};
+
+#endif //DISPLAY_TEST_H
