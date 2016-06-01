9a8c6ba9ded605e69bfe398a7eaeeda3f1eddf59
Marco Randazzo <marco.randazzo@iit.it>
2016-06-01 18:06:48 +0200

    improved ikart odometry, orientation of odom reference frame changed

--- modules/baseControl/ikart/ikart_odometry.cpp
+++ modules/baseControl/ikart/ikart_odometry.cpp
@@ -167,7 +167,7 @@ void iKart_Odometry::compute()
     // -------------------------------------------------------------------------------------
 
     //compute the orientation. odom_theta is expressed in radians
-    odom_theta = geom_r*(enc[0]+enc[1]+enc[2])/(3*geom_L);
+    odom_theta = -geom_r*(enc[0]+enc[1]+enc[2])/(3*geom_L);
 
     //build the kinematics matrix
     yarp::sig::Matrix kin;
@@ -225,8 +225,8 @@ void iKart_Odometry::compute()
     base_vel_theta = ikart_cart_vels[2];
     base_vel_lin   = sqrt(odom_vel_x*odom_vel_x + odom_vel_y*odom_vel_y);
     
-    odom_vel_x      = odom_cart_vels[1];
-    odom_vel_y      = odom_cart_vels[0];
+    odom_vel_x      = odom_cart_vels[0];
+    odom_vel_y      = odom_cart_vels[1];
     odom_vel_theta  = odom_cart_vels[2];
   
     //these are not currently used
