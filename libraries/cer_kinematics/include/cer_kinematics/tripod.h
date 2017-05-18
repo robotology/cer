b55baec7e4685cfa4cefae93d2743764aa636dcf
Ugo Pattacini <ugo.pattacini@iit.it>
2017-05-18 17:25:02 +0200

    fixed comments

--- libraries/cer_kinematics/include/cer_kinematics/tripod.h
+++ libraries/cer_kinematics/include/cer_kinematics/tripod.h
@@ -94,7 +94,7 @@ public:
      * @param lll    the three elongations ([m]).
      * @param p      the computed 3d position of the platform center
      *               ([m]).
-     * @param u      the 3 components orientation (axis,angle) given
+     * @param u      the 4 components orientation (axis,angle) given
      *               in [rad].
      * @return true/false on success/failure.
      */
@@ -129,7 +129,7 @@ public:
      * (axis*angles). 
      * 
      * @param zd     desired heave ([m]).
-     * @param ud     the desired orientation given as 3 components 
+     * @param ud     the desired orientation given as 4 components 
      *               vector (axis,angle) expressed in [rad].
      * @param lll    the solved three elongations ([m]).
      * @return true/false on success/failure.
