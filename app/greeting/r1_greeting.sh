52bf5ef5ec7446bb5a002c6c8e0fec1f35740e3c
Raffaele Colombo <colombraf@gmail.com>
2023-10-20 09:49:04 +0200

    sped up greeting

--- app/greeting/r1_greeting.sh
+++ app/greeting/r1_greeting.sh
@@ -333,12 +333,10 @@ sequence_2() {
 
 say_hi() {
     
-    echo "ctpq time 3.0 off 0 pos (17.25 48.0 44.2 94.5 0.0)" | yarp rpc /ctpservice/right_arm/rpc
-    echo "ctpq time 2.0 off 0 pos (17.25 40.0 44.2 81.9 0.0)" | yarp rpc /ctpservice/right_arm/rpc
     echo "ctpq time 2.0 off 0 pos (17.25 48.0 44.2 94.5 0.0)" | yarp rpc /ctpservice/right_arm/rpc
-    echo "ctpq time 2.0 off 0 pos (17.25 40.0 44.2 81.9 0.0)" | yarp rpc /ctpservice/right_arm/rpc
-    echo "ctpq time 2.0 off 0 pos (17.25 48.0 44.2 94.5 0.0)" | yarp rpc /ctpservice/right_arm/rpc
-    echo "ctpq time 2.0 off 0 pos (17.25 40.0 44.2 81.9 0.0)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.5 off 0 pos (17.25 40.0 44.2 81.9 0.0)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.5 off 0 pos (17.25 48.0 44.2 94.5 0.0)" | yarp rpc /ctpservice/right_arm/rpc
+    echo "ctpq time 1.5 off 0 pos (17.25 40.0 44.2 81.9 0.0)" | yarp rpc /ctpservice/right_arm/rpc
     arms_down	
 }
 
