903bdec0fa97692eec69d3a1697adf6241e76a18
Raffaele Colombo <colombraf@gmail.com>
2023-12-04 16:43:57 +0100

    added rand

--- modules/gesticulate/gesticulateThread.cpp
+++ modules/gesticulate/gesticulateThread.cpp
@@ -61,18 +61,19 @@ void GesticulateThread::run()
     {
         Bottle motion1{"ctpq time 3.0 off 0 pos (29.44 11.60 -10.11 73.74 62.99 0 -0.02 0.23)"};
         Bottle motion2{"ctpq time 3.0 off 0 pos (29.36 9.05 8.53 71.02 61.04 0 -0.06 -0.29)"};
-        m_rpc_right.write(motion1);
-        m_rpc_left.write(motion2);
-        Time::delay(3.0);
+        
+        if(rand() % 2) m_rpc_right.write(motion1);
+        if(rand() % 2) m_rpc_left.write(motion2);
+        Time::delay(4.0);
 
         audio_is_playing = (unsigned int)player_status->get(1).asInt64() > 0;
         if(audio_is_playing)
         {
-            Bottle motion3{"ctpq time 2.0 off 0 pos (29.44 11.60 25.40 70.14 63.08 0 -0.11 0.07)"};
-            Bottle motion4{"ctpq time 2.0 off 0 pos (29.36 9.05 -10.02 50.27 32.8217 0 -0.02 -0.36)"};
-            m_rpc_right.write(motion3);
-            m_rpc_left.write(motion4);
-            Time::delay(2.0);
+            Bottle motion3{"ctpq time 3.0 off 0 pos (29.44 11.60 25.40 70.14 63.08 0 -0.11 0.07)"};
+            Bottle motion4{"ctpq time 3.0 off 0 pos (29.36 9.05 -10.02 50.27 32.8217 0 -0.02 -0.36)"};
+            if(rand() % 2) m_rpc_right.write(motion3);
+            if(rand() % 2) m_rpc_left.write(motion4);
+            Time::delay(3.0);
         }
 
         Bottle armHome{"ctpq time 3.0 off 0 pos (-9.0 15.0 -10.0 50.5 0.0 0.0 -0.0 -0.0)"};
